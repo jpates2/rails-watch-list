4c1362da94bd1bdd5ef607c0562bc7b6383a3d97
Jess Pates <jesspates@hotmail.co.uk>
2022-11-17 14:50:59 +0100

    list controller and view done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # resources :restaurants, only: [:new, :create, :index, :show] do
+  #   resources :reviews, only: [:new, :create, :index, :show]
+
+  resources :lists, only: [:new, :create, :index, :show]
 end
