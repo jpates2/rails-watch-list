c98bd2c014f2fa48afbd5cfc70d06d1234f77668
Jess Pates <jesspates@hotmail.co.uk>
2022-12-29 15:26:54 +0000

    navbar updates

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
-  validates :photo, presence: true
+  # validates :photo, presence: true
   has_many :movies, through: :bookmarks
   has_one_attached :photo
 end
