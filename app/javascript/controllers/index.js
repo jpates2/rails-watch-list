d477665be9cfda3b345c865ca24f8d274286f6d8
Jess Pates <jesspates@hotmail.co.uk>
2023-01-09 21:15:42 +0000

    outstanding updates

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import NavbarController from "./navbar_controller.js"
+import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
+
+import ScrollToController from "./scroll_to_controller"
+application.register("scroll-to", ScrollToController)
---
+++ app/javascript/controllers/scroll_to_controller.js
@@ -0,0 +1,11 @@
+// import { Controller } from "@hotwired/stimulus"
+// import { Application } from 'stimulus'
+// import { ScrollTo } from 'stimulus-scroll-to'
+
+// // Connects to data-controller="scroll-to"
+// export default class extends Controller {
+//   connect() {
+//     const application = Application.start()
+//     application.register("scroll-to", ScrollTo)
+//   }
+// }
